1dfa8d1cfbe998f79319358f7fd46e13f324d88c
JonasKrukenberg <jonas.krukenberg@haw-hamburg.de>
2020-07-03 13:41:43 +0200

    adornoWinner, collatz

--- klausurvorbereitung-jonas/src/main/java/de/hawh/kahlbrandt/ss2018bai2pm2exam/A03.java
+++ klausurvorbereitung-jonas/src/main/java/de/hawh/kahlbrandt/ss2018bai2pm2exam/A03.java
@@ -3,41 +3,39 @@ package de.hawh.kahlbrandt.ss2018bai2pm2exam;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class A03 {
 
     public static int adornoWinner(Path path) {
         try {
-            int row = -1;
-            int index = -1;
-            final String[] lines = Files.lines(path).toArray(String[]::new);
-            for (int i = 0; i < lines.length; i++) {
-                if (lines[i].indexOf("sich") > index) {
-                    row = i;
-                }
-            }
-            return row;
+            List<Integer> positions = Files.lines(path)
+                    .map(l -> l.indexOf("sich"))
+                    .collect(Collectors.toList());
+            Optional<Integer> max = positions.stream()
+                    .max(Integer::compareTo);
+            return max.map(e -> e != -1 ? positions.indexOf(e) : e)
+                    .orElse(-1);
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new IllegalArgumentException("File not found!", e);
         }
-        return -1;
     }
 
-    public static Stream<Integer> collatz(int n) {
-        if (n <= 0) {
+    public static Stream<Integer> collatz(int start) {
+        if (start <= 0) {
             throw new RuntimeException("n has to be >= 0");
         }
-        return Stream.iterate(n, next -> next > 0, A03::c);
-    }
-
-    private static int c(int n) {
-        if (n == 0) {
-            return 0;
-        }
-        if (n % 2 == 0) {
-            return c(n - 1) / 2;
-        }
-        return 3 * c(n - 1);
+        return Stream.iterate(start, c -> c != 1, n -> {
+            if (n == 0) {
+                return 0;
+            }
+            if (n % 2 == 0) {
+                return n / 2;
+            }
+            return 3 * n + 1;
+        });
     }
 }
